cd72235f10c489da5612e26aa9f797ea37bf670d
323266536 <phillyp.henning@rbc.com>
2019-08-09 09:33:53 -0400

    bookController added

---
+++ controllers/booksController.js
@@ -0,0 +1,84 @@
+function bookController(Book){
+    function post(req,res){
+        const book = new Book(req.body);
+    
+        console.log(book);
+        book.save();
+        return res.status(201).json(book)
+        };
+    
+    function get(req, res) {
+        //These two items are the same, however in ES6 the introduced destructuring which allows us to pull a value out of another object granted they have the same name.
+        // --old way--
+        //const query = req.query
+        // --new way (destructuring)--
+        //const {query} = req
+        const query = {};
+        if(req.query.language){
+            query.language = req.query.language;
+        }
+
+        // Book is being exported from the bookModel file which makes it callable here.
+        Book.find(query, (err, books) => {
+            if(err){
+                return res.send(err);
+            }
+            return res.json(books);
+        });
+    }
+
+    function getById(req, res){ res.json(req.book); }
+
+    function put(req, res){
+        // This is a great example of the Middleware. As you can see book is already available within the request. This is because of the above middleware. 
+        const { book } = req;
+        book.title = req.body.title;
+        book.author = req.body.author;
+        book.country = req.body.country;
+        book.language = req.body.language;
+        book.pages = req.body.pages;
+        book.year = req.body.year;
+
+        req.book.save((err) =>{
+            if(err){
+                return res.send(err);
+            }
+            return res.json(book);
+        });
+    }
+
+    function patch(req, res){
+        const { book } = req;
+
+        // Ids are not to be updated.
+        if (req.body._id){
+            delete req.body._id;
+        }
+        
+        Object.entries(req.body).forEach((item) => {
+            const key = item[0];
+            const value = item[1];
+            book[key] = value;
+        });
+
+        req.book.save((err) =>{
+            if(err){
+                return res.send(err);
+            }
+            return res.json(book);
+        });
+    }
+
+    function bookDelete(req, res){
+        req.book.remove((err) =>{
+            if (err){
+                res.send(err);
+            }
+            return res.sendStatus(204);
+        });
+    }
+
+    return {post, get, getById, put, patch, bookDelete};
+}
+
+module.exports = bookController;
--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,41 +1,14 @@
 const express = require('express');
+const booksController = require('../controllers/booksController'); // TODO : Add project level direcotry string to remove static directory strings.
 
 function routes(Book){
     // Gives us access to our API's express Router routes
     const bookRouter = express.Router();
+    const controller = booksController(Book);
+
     bookRouter.route('/books')
-
-    // POST - Add book
-    .post((req,res)=>{
-    const book = new Book(req.body);
-
-    console.log(book);
-    book.save();
-    return res.status(201).json(book)
-    })
-
-    //GET - Display all books
-    .get((req, res) => {
-
-        //These two items are the same, however in ES6 the introduced destructuring which allows us to pull a value out of another object granted they have the same name.
-        // --old way--
-        //const query = req.query
-        // --new way (destructuring)--
-        //const {query} = req
-
-        const query = {};
-        if(req.query.language){
-            query.language = req.query.language;
-        }
-
-        // Book is being exported from the bookModel file which makes it callable here.
-        Book.find(query, (err, books) => {
-            if(err){
-                return res.send(err);
-            }
-            return res.json(books);
-        });
-    });
+    .post(controller.post) //POST - Add new book
+    .get(controller.get) // GET - Gets all books
 
     // Creates a middleware function specifically for the '/book/:bookId' route. This function does a small piece of work then calls next() which allows the function call to continue. 
     bookRouter.use('/books/:bookId', (req, res, next) =>{
@@ -51,61 +24,11 @@ function routes(Book){
         });
     });
 
-    // GET - Find book by ID
     bookRouter.route('/books/:bookId')
-    .get((req, res) => { res.json(req.book); })
-
-    // PUT - PUT IN NEW INFO ABOUT A ALREADY EXISTING BOOK
-    .put((req, res) => {
-        // This is a great example of the Middleware. As you can see book is already available within the request. This is because of the above middleware. 
-        const { book } = req;
-        book.title = req.body.title;
-        book.author = req.body.author;
-        book.country = req.body.country;
-        book.language = req.body.language;
-        book.pages = req.body.pages;
-        book.year = req.body.year;
-
-        req.book.save((err) =>{
-            if(err){
-                return res.send(err);
-            }
-            return res.json(book);
-        });
-    })
-    
-    // PATCH - Single book, single item updating.
-    .patch((req, res) =>{
-        const { book } = req;
-
-        // Ids are not to be updated.
-        if (req.body._id){
-            delete req.body._id;
-        }
-        
-        Object.entries(req.body).forEach((item) => {
-            const key = item[0];
-            const value = item[1];
-            book[key] = value;
-        });
-
-        req.book.save((err) =>{
-            if(err){
-                return res.send(err);
-            }
-            return res.json(book);
-        });
-    })
-
-    // DELETE - Deletes a single book
-    .delete((req, res) => {
-        req.book.remove((err) =>{
-            if (err){
-                res.send(err);
-            }
-            return res.sendStatus(204);
-        });
-    });
+    .get(controller.getById) // GET - Find book by ID
+    .put(controller.put) // PUT - PUT IN NEW INFO ABOUT A ALREADY EXISTING BOOK
+    .patch(controller.patch)    // PATCH - Single book, single item updating.
+    .delete(controller.bookDelete);    // DELETE - Deletes a single book
 
     return bookRouter;
 }
