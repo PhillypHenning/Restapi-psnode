395876ea9d45a8141da507c1a67b9af1907df73a
323266536 <phillyp.henning@rbc.com>
2019-08-09 09:48:55 -0400

    post test added

--- tests/bookControllerTests.js
+++ tests/bookControllerTests.js
@@ -1,3 +1,25 @@
 const should = require('should');
 const sinon = require('sinon');
 const bookController = require('../controllers/booksController');
+
+describe('Book Controller Tests:', ()=>{
+    const Book = function (book) { this.saave = () => {}};
+
+    const req = {
+        body: {
+            author: 'testy'
+        }
+    };
+
+    const res = {
+        status: sinon.spy(),
+        send: sinon.spy(),
+        json: sinon.spy()
+    };
+
+    const controller = bookController(Book);
+    controller.post(req, res);
+
+    res.status.calledWith(400).should.equal(true, `Bad status ${res.status.args[0][0]}`);
+    res.send.calledWith('Title is required').should.equal(true);
+})
